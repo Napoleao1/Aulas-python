5ab0b0198782bceca15a2beafbc968016510a6d5
ernani <ernaninandoxd@gmail.com>
2026-04-02 20:13:33 -0300

    subindo aulas

---
+++ desafio.py
@@ -0,0 +1,12 @@
+festa = []
+
+festa.append('maria')
+festa.append('ernani')
+festa.append('lucas')
+
+
+festa.insert(0, "VIP")
+
+festa.remove('maria')
+
+print(festa)
---
+++ desafio2.py
@@ -0,0 +1,8 @@
+fila = ["ana", "carlos", "joao"]
+
+
+fila.pop(0)
+
+fila.append("pedro")
+
+print(fila)
